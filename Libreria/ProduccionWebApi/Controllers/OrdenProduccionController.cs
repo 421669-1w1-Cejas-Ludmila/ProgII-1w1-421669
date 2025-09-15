6213294d324b1bd0a648284e8fc64230fa0f410d
421669-1w1-Cejas-Ludmila <421669@tecnicatura.frc.utn.edu.ar>
2025-09-14 21:49:06 -0300

    Add files via upload
    Agregar Web API de libreria

---
+++ Libreria/ProduccionWebApi/Controllers/OrdenProduccionController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProduccionBack.Entities;
+using ProduccionBack.Services;
+
+namespace ProduccionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdenProduccionController : ControllerBase
+    {
+        private IProduccionService service;
+
+        public OrdenProduccionController()
+        {
+            service = new ProduccionService();
+        }
+
+        // GET: api/<OrdenProduccionController>
+        [HttpGet("componentes")]
+        public IActionResult Get()
+        {
+            return Ok(service.ConsultarComponentes());
+        }
+
+        //GET: Ordenes
+        [HttpGet("ordenes")]
+        public IActionResult GetOrdenes([FromQuery] DateTime? fecha, [FromQuery] string? estado)
+        {
+            try
+            {
+                var lista=service.ConsultarOrdenes(fecha, estado);
+                if (lista.Count == 0) 
+                    return NotFound("No se encontraron órdenes con esos criterios!!!");
+                return Ok(lista);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "No se pudo consultar las ordenes!!!");
+            }
+        }
+
+        // POST api/<OrdenProduccionController>
+        [HttpPost]
+        public IActionResult Post([FromBody] OrdenProduccion orden)
+        {
+            try { 
+                if(orden == null)
+                {
+                    return BadRequest("Se esperaba una orden de producción completa");
+                }
+                if (service.RegistrarProduccion(orden))
+                    return Ok("Orden registrada con éxito!");
+                else
+                    return StatusCode(500, "No se pudo registrar la orden!");
+            }
+            catch (Exception )
+            {
+                return StatusCode(500, "Error interno, intente nuevamente!");
+            }
+        }
+    }
+}
